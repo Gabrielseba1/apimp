3076c68d3beec399a294848bd105395c9d88e83e
Gabrielseba1 <44471823+Gabrielseba1@users.noreply.github.com>
2024-07-28 23:56:36 -0300

    Update paymentController.js

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,6 +1,5 @@
 const mercadopago = require('mercadopago');
 
-
 mercadopago.configure({
   access_token: process.env.MERCADOPAGO_ACCESS_TOKEN
 });
@@ -26,12 +25,40 @@ exports.gerarPagamento = async (req, res) => {
       pending: `${req.protocol}://${req.get('host')}/pending`
     },
     auto_return: 'approved',
-    external_reference: `ref-${Date.now()}` 
+    external_reference: `ref-${Date.now()}`,
+    payment_methods: {
+      excluded_payment_types: [
+        { id: 'credit_card' },
+        { id: 'ticket' }
+      ],
+      installments: 1,
+      default_payment_method_id: 'pix'
+    }
   };
 
   try {
     const response = await mercadopago.preferences.create(preference);
-    res.json({ id: response.body.id, init_point: response.body.init_point, external_reference: preference.external_reference });
+    const init_point = response.body.init_point;
+
+  
+    const payment = await mercadopago.payment.create({
+      transaction_amount: parseFloat(valor),
+      payment_method_id: 'pix',
+      payer: {
+        email: 'wedledev@gmail.com.com'  
+      }
+    });
+
+    const pix_qr_code = payment.body.point_of_interaction.transaction_data.qr_code_base64;
+    const pix_copy_paste = payment.body.point_of_interaction.transaction_data.qr_code;
+
+    res.json({
+      id: response.body.id,
+      init_point: init_point,
+      external_reference: preference.external_reference,
+      pix_qr_code: pix_qr_code,
+      pix_copy_paste: pix_copy_paste
+    });
   } catch (error) {
     res.status(500).send(`Erro ao criar preferência de pagamento: ${error.message}`);
   }
